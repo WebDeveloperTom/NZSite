20614696c7df5b34cf494e947bf37b1ab9c7cc37
WebDeveloperTom <tom@webdevelopertom.com>
2018-10-14 14:36:56 +1300

    Updated some locations

--- scripts/index.js
+++ scripts/index.js
@@ -9,42 +9,42 @@ function initMap() {
   const attractionInfo = [
     {
       coords: { lat: -36.9938006, lng: 174.8836618 },
-      content: "<h1>Rainbow's End</h1>"
+      content: "<p>Rainbow's End</p>"
     },
     {
       coords: { lat: -38.0830395, lng: 176.1882802 },
-      content: "<h1>ZORB™ Rotorua</h1>"
+      content: "<h3>ZORB™ Rotorua</h3>"
     },
     {
       coords: { lat: -45.03092, lng: 168.660166 },
-      content: "<h1>AJ Hackett Bungy</h1>"
+      content: "<h3>AJ Hackett Bungy</h3>"
     },
     {
-      coords: { lat: -38.110432, lng: 176.221969 },
-      content: "<h1>Skyline Rotorua</h1>"
+      coords: { lat: -37.8723, lng: 175.6834 },
+      content: "<p> Hobbiton Movie set</p>"
     },
+
     {
       coords: { lat: -39.3044, lng: 175.5269 },
-      content: "<h1>Turoa Skifield</h1>"
+      content: "<h3>Turoa Skifield</h3>"
     },
     {
       coords: { lat: -41.2784, lng: 174.7767 },
-      content: "<h1>The Beehive</h1>"
+      content: "<h3>The Beehive</h3>"
     },
     {
       coords: { lat: -43.5848, lng: 172.7077 },
-      content: "<h1>Christchurch Gondola</h1>"
+      content: "<h3>Christchurch Gondola</h3>"
     },
     {
       coords: { lat: -45.8667, lng: 170.6667 },
-      content: "<h1>The Otago Peninsula</h1>"
+      content: "<h3>The Otago Peninsula</h3>"
     },
     {
       coords: { lat: -46.4095, lng: 168.3804 },
-      content: "<h1>Bill Richardson Transport World</h1>"
+      content: "<h3>Bill Richardson Transport World</h3>"
     }
   ];
-  //-38.110432,176.221969
 
   //loop over the attraction info and create a marker for each.
   attractionInfo.forEach(addMarker);
@@ -71,8 +71,11 @@ function initMap() {
         marker.addListener("mouseover", function() {
           infoWindow.open(map, marker);
         });
+
         marker.addListener("mouseout", function() {
-          infoWindow.close(map, marker);
+          setTimeout(function() {
+            infoWindow.close(map, marker);
+          }, 2000);
         });
       }
     }, 2000);
